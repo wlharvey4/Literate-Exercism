b0909b302d5e72ad0e2bfce203c0485d2f00d006
wlharvey <pinecone062@gmail.com>
2019-01-14 20:17:01 -0800

    Exercism.org version 0.0.5 Update hello_world.py and test

--- python/hello-world/hello_world_test.py
+++ python/hello-world/hello_world_test.py
@@ -6,9 +6,9 @@ import hello_world
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
 class HelloWorldTest(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello_world.hello(), 'Hello, World!')
+  def test_hello(self):
+    self.assertEqual(hello_world.hello(), 'Hello, World!')
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
